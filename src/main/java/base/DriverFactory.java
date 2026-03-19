ecdc93b8168fdd7f2972964cc7920ff84395fd23
gundrenagi <gundrenagi@gmail.com>
2026-03-19 23:45:22 +0800

    Add ChromeOptions import to DriverFactory

--- src/main/java/base/DriverFactory.java
+++ src/main/java/base/DriverFactory.java
@@ -2,8 +2,7 @@ package base;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-
-import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class DriverFactory {
 	
